df46f811d581b3ad111be7d9914c004b62657a1e
Scott Kennedy <scottyzen@gmail.com>
2018-10-09 22:30:59 +0100

    added more data

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+// server.js
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log('server started ' + port)
+console.log('server started ' + port);
