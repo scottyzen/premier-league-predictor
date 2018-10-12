9fedf60e6edeb6d4bf2c2417dbff5560eb311e9a
Scott Kennedy <scottyzen@gmail.com>
2018-10-12 01:32:00 +0100

    Added google analitics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
+import VueAnalytics from "vue-analytics";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "UA-127390099-1"
+});
 
 new Vue({
   router,
